7fed6e45f05e79904729ab468a74c33faf19c6f3
Ahmer Malik <ahmer9800@gmail.com>
2017-12-11 11:51:04 -0600

    add working googlemaps component, thank you paul

--- src/components/googlemaps.jsx
+++ src/components/googlemaps.jsx
@@ -1,23 +1,49 @@
 import React from 'react';
 import Core from '../core/yelp_api';
-import { GoogleMap, Marker } from "react-google-maps"
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
 import {Link} from 'react-router-dom';
 
+
+var MyMapComponent = withScriptjs(withGoogleMap(function (props) {
+    return (
+        <GoogleMap
+            defaultZoom={8}
+            defaultCenter={{lat: 29.783204, lng: -95.71791}}
+        >
+            {props.isMarkerShown && <Marker position={{lat: 29.783204, lng: -95.71791}}/>}
+        </GoogleMap>
+    )
+}))
+
+class MyMapComponent2 extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render () {
+        return (
+            <GoogleMap
+                defaultZoom={8}
+                defaultCenter={{lat: 29.783204, lng: -95.71791}}
+            >
+                {this.props.isMarkerShown && <Marker position={{lat: 29.783204, lng: -95.71791}}/>}
+            </GoogleMap>
+        )
+    }
+}
+
+var Wrapped = withScriptjs(withGoogleMap(MyMapComponent2))
 
 export default class Directions extends React.Component{
     render(props){
         return(
             <div>
-                MyMapComponent = (props) =>
-                <GoogleMap
-                    defaultZoom={8}
-                    defaultCenter={{ lat: -34.397, lng: 150.644 }}
-                >
-                    {props.isMarkerShown && <Marker position={{ lat: -34.397, lng: 150.644 }} />}
-                </GoogleMap>
-
+                <MyMapComponent isMarkerShown={true}
+                                googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+                                loadingElement={<div style={{ height: `100%` }} />}
+                                containerElement={<div style={{ height: `400px` }} />}
+                                mapElement={<div style={{ height: `100%` }} />}/>
             </div>
-                <MyMapComponent isMarkerShown />
 
         )
 
